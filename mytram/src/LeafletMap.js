82078159153867dd01c643b5af8babb10c125f06
Jari Hiekkapelto <jari.hiekkapelto@gmail.com>
2019-08-23 09:14:34 +0300

    yritystä saada liikettä

--- mytram/src/LeafletMap.js
+++ mytram/src/LeafletMap.js
@@ -3,20 +3,26 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { subscribe } from 'mqtt-react'
 
 
-const mapCenterPosition = { lat: 60.19501135150039, lng: 24.943557594049953 }
+const mapInitialCenter = { lat: 60.19501135150039, lng: 24.943557594049953 }
 
 
 
 const LeafletMap = (props) => {
 
   const [trams, setTrams] = useState([])
-  const [showTrams, setShowTrams] = useState([])  
-   
+  //const [showTrams, setShowTrams] = useState([])
+
+
+
+
   let newData = props.data[0]
-  if (newData !== undefined) {
-    console.log(new Date())
+  if (newData) {
+
     let VP = newData.VP
-    if (!trams.map(tram => tram.veh).includes(VP.veh)){
+
+       
+     
+      if (!trams.map(tram => tram.veh).includes(VP.veh)) {
       setTrams(trams.concat(
         {
           linja: VP.desi,
@@ -24,50 +30,58 @@ const LeafletMap = (props) => {
           lat: VP.lat,
           long: VP.long
         }
-      ))
-    } /* else {
-       
-      setTrams(trams.map(
-        tram => tram.veh === VP.veh ?
-          {
-            linja: VP.desi,
-            veh: VP.veh,
-            lat: VP.lat,
-            long: VP.long
-          }
-          : tram))   
-    }   */
+      )) 
+
+    } else {
+
+
+      /* setTrams(trams.map(
+      tram => tram.veh === VP.veh ?
+        {
+          linja: VP.desi,
+          veh: VP.veh,
+          lat: VP.lat,
+          long: VP.long
+        }
+        : tram)) */
+      // console.log(new Date())
+     }  
   }
 
-  console.log(trams)
-  console.log(newData)  
+  //console.log(trams)
+  //console.log(newData)
 
+  const MarkerFactory = () => {
+    return (
+      trams.map(tram =>
+        <Marker
+          key={tram.veh}
+          position={{
+            lat: tram.lat,
+            lng: tram.long
+          }}
+        >
+          <Popup closeButton={false}>
+            {tram.linja}
+          </Popup>
+        </Marker>
+      )
+
+    )
+  }
 
 
   return (
     <div className="App">
-      <Map id="mapid" center={mapCenterPosition} zoom={12}>
+      <Map id="mapid" center={mapInitialCenter} zoom={12}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={mapCenterPosition}>
+        <Marker position={mapInitialCenter}>
           <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
         </Marker>
-        {/*  {MarkerFactory()} */}
-        {showTrams.map(tram =>
-          <Marker
-            key={tram.veh}
-            position={{
-              lat: tram.lat,
-              lng: tram.long
-            }}
-          >
-            <Popup closeButton={false}>
-              {tram.linja}
-            </Popup>
-          </Marker>
-        )}
+        {MarkerFactory()}
       </Map>
     </div>
   );
