dc686e808547c02b9444c2fff9255ee4a50ffa8d
Jari Hiekkapelto <jari.hiekkapelto@gmail.com>
2019-09-21 11:04:52 +0300

    Native templates

---
+++ KusRatikka/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+ const App = () => {
+ 
+  //console.log('hello')
+ 
+/* const timer = setInterval(()=>{
+    setIs(!is)
+}, 1000)
+ */
+  return (
+    <View style={styles.container}>
+      <Text>Open upp  to fart working on your räp nr. !</Text>
+      
+    </View>
+  );
+ 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App
---
+++ missRatikka/components/StyledText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export function MonoText(props) {
+  return (
+    <Text {...props} style={[props.style,
+     /*    { fontFamily: 'space-mono' } */
+      ]} />
+  );
+}
