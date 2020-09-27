bdcbde045ae8fe922df6ca3a484992fb2d0f0a2a
jhiekkap <jari.hiekkapelto@gmail.com>
2020-09-27 09:09:48 +0300

    refactor

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const mqtt = require('mqtt')
-
+const mqtt = require('mqtt') 
 const cors = require('cors')
 
 app.use(cors())
@@ -18,26 +17,21 @@ client.on('connect', function () {
   })
 })
 
-client.on('message', function (topic, message) { 
-
-  let VP = JSON.parse(message.toString()).VP
-
+client.on('message', function (topic, message) {  
+  let VP = JSON.parse(message.toString()).VP  
   if (!trams.map(tram => tram.VP.veh).includes(VP.veh)) {
     trams.push({ topic, VP, updateTime: new Date() })
   } else {
     trams = trams.map(tram =>
       tram.VP.veh === VP.veh ? { topic, VP, updateTime: new Date() } : tram
     )
-  }
-
-  trams = trams.filter(tram => new Date() - tram.updateTime < 50000)  
-
+  } 
+  trams = trams.filter(tram => new Date() - tram.updateTime < 50000)   
 })
   
 
 app.get('/trams', async (req, res) => {
-  console.log(new Date())
-
+  console.log(new Date()) 
   try {
     //res.json(trams.filter(tram => tram.VP.desi === '10'))
     res.json(trams)
@@ -46,8 +40,6 @@ app.get('/trams', async (req, res) => {
     console.log('error: ', error)
   }
 }) 
-
-
-
+  
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log('Server running in port: ' + PORT))
